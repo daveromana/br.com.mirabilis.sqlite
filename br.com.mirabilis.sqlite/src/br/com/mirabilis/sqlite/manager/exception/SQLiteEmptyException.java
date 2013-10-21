7580b1bec74b02c582e6e00ebc329a4da14617b4
Rodrigo <rodrigosimoesrosa@gmail.com>
2013-10-21 18:07:18 -0200

    SQLiteEmptyException inclusão

---
+++ br.com.mirabilis.sqlite/src/br/com/mirabilis/sqlite/manager/exception/SQLiteEmptyException.java
@@ -0,0 +1,25 @@
+package br.com.mirabilis.sqlite.manager.exception;
+
+/**
+ * Exception of control result of query when that is empty.
+ * @author Rodrigo Simões Rosa.
+ */
+public class SQLiteEmptyException extends Exception {
+
+	/**
+	 * Serialization
+	 */
+	private static final long serialVersionUID = 5730852278529610005L;
+
+	public SQLiteEmptyException(String msg) {
+		super(msg);
+	}
+	
+	public SQLiteEmptyException() {
+		this("The result of query in sqlite is empty");
+	}
+	
+	public SQLiteEmptyException(Throwable throwable){
+		super(throwable);
+	}
+}
