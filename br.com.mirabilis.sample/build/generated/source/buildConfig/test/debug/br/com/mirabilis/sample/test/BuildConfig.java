70d446e03bd8f65f212797797a5992a18a6b977c
rodrigosimoesrosa <rodrigosimoesrosa@gmail.com>
2015-03-10 23:36:18 -0300

    Sample

--- br.com.mirabilis.sample/build/generated/source/buildConfig/test/debug/br/com/mirabilis/sample/test/BuildConfig.java
+++ br.com.mirabilis.sample/build/generated/source/buildConfig/test/debug/br/com/mirabilis/sample/test/BuildConfig.java
@@ -5,7 +5,7 @@ package br.com.mirabilis.sample.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "br.com.mirabilis.sample.test";
+  public static final String APPLICATION_ID = "br.com.mirabilis.sample.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = -1;
--- br.com.mirabilis.sqlite/build/generated/source/buildConfig/debug/br/com/mirabilis/sqlite/BuildConfig.java
+++ br.com.mirabilis.sqlite/build/generated/source/buildConfig/debug/br/com/mirabilis/sqlite/BuildConfig.java
@@ -5,7 +5,7 @@ package br.com.mirabilis.sqlite;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "br.com.mirabilis.sqlite";
+  public static final String APPLICATION_ID = "br.com.mirabilis.sqlite";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
